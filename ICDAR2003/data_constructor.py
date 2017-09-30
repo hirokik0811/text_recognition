677fbad78680307c5b8c833f4c60324c156b8531
Hiroki <kwibunbun@gmail.com>
2017-09-30 00:01:43 -0400

    Changed resize method
    Resize character images by adding reflection borders

--- ICDAR2003/data_constructor.py
+++ ICDAR2003/data_constructor.py
@@ -14,7 +14,11 @@ e = xml.etree.ElementTree.parse('char.xml').getroot()
 for image in e:
     img = cv2.imread(image.get('file'))
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    resized = cv2.resize(gray_img, (32, 32))
+    border_width = max([0, int((gray_img.shape[0]-gray_img.shape[1])/2)])
+    border_height = max([0, int((gray_img.shape[1]-gray_img.shape[0])/2)])
+    bordered = cv2.copyMakeBorder(gray_img, border_height, border_height, border_width, border_width,
+                                 borderType = cv2.BORDER_REFLECT_101)
+    resized = cv2.resize(bordered, (32, 32))
     label = np.zeros(62)
     label[char_list.find(image.get('tag'))] = 1.0
     image_data = np.array([])
--- char74k/data_constructor.py
+++ char74k/data_constructor.py
@@ -25,7 +25,11 @@ for i in range(0, 62):
             filename += ('-000'+ str(j) + '.png')
         img = cv2.imread(filename)
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        resized = cv2.resize(gray_img, (32, 32))
+        border_width = max([0, int((gray_img.shape[0]-gray_img.shape[1])/2)])
+        border_height = max([0, int((gray_img.shape[1]-gray_img.shape[0])/2)])
+        bordered = cv2.copyMakeBorder(gray_img, border_height, border_height, border_width, border_width
+                                      , borderType = cv2.BORDER_REFLECT_101)
+        resized = cv2.resize(bordered, (32, 32))
         image_data = np.array([])
         image_data = np.append(image_data, resized)
         image_data = np.append(image_data, label)
