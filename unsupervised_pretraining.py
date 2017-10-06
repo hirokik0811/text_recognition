24e8b3d67c83f0c007193c8637797bb56d16bd49
Hiroki <kwibunbun@gmail.com>
2017-10-06 01:23:13 -0400

    Added unsupervised_pretraining.py
    Added the file.

---
+++ unsupervised_pretraining.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 4, 2017
+
+@author: hiroki
+'''
+
+import numpy as np
+
+"""
+Unsupervised feature extraction.
+First we want to obtain 8x8 patches from character image dataset.
